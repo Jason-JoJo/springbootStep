2c8cdfa2b7be0fa4e5cc10fb65b167c044507727
Jason-JoJo <pilaco0822@gmail.com>
2020-12-27 22:05:59 +0800

    Step 23

---
+++ demo/src/main/java/com/example/demo/controller/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+//	New Code
+//
+//	`1. auth.inMemoryAuthentication()
+//	2.             .passwordEncoder(NoOpPasswordEncoder.getInstance())
+//	3.         		.withUser("in28Minutes").password("dummy")`
+//
+//	Old Code
+//
+//	`1. auth.inMemoryAuthentication().withUser("in28Minutes").password("dummy")`
+	
+	
+	//Creat user
+	@Autowired
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()			
+			.withUser("jason").password("{noop}1234").roles("USER", "ADMIN");
+			
+	}
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/login").permitAll()
+                .antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
+                .formLogin();
+      
+	}
+}
