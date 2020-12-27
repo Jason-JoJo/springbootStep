2bc79a876e5d5b655c0027fdef62809db712773f
Jason-JoJo <pilaco0822@gmail.com>
2020-12-27 22:54:23 +0800

    Step 24

--- demo/src/main/java/com/example/demo/controller/TodolistController.java
+++ demo/src/main/java/com/example/demo/controller/TodolistController.java
@@ -7,23 +7,18 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.demo.model.Todo;
-import com.example.demo.service.LoginService;
 import com.example.demo.service.TodoService;
 
 @Controller
